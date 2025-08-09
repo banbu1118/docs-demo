e929bce264e95e794effce1ae568dfce090521b8
banbu1118 <1902802324@qq.com>
2025-08-09 18:25:38 +0800

    update

---
+++ .vitepress/config.mjs
@@ -0,0 +1,81 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  title: "半步的博客",
+  description: "学习记录",
+  themeConfig: {
+    logo: '/logo.png',
+    outlineTitle: '目录',
+    outline: [2, 6],
+
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家目录', link: '/' },
+      { text: '示例', link: '/markdown-examples' },
+      { text: '百度', link: 'https://www.baidu.com' },
+      {
+        text: 'home',
+        items: [
+          { text: 'mardown-examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+          { text: 'VuePress', link: 'https://vitepress.vuejs.org' }
+        ]
+      }
+    ],
+
+    sidebar: [
+      {
+        text: '示例',
+        items: [
+          { text: 'Markdown 演示', link: '/markdown-examples' },
+          { text: 'Runtime API 演示', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '示例2',
+        items: [
+          { text: 'Markdown 演示', link: '/markdown-examples' },
+          { text: 'Runtime API 演示', link: '/api-examples' }
+        ]
+      },
+      {
+        text: '运维',
+        items: [
+          { text: 'pve导入cloud-image创建虚拟机', link: '/ops/pve导入cloud-image创建虚拟机/pve导入cloud-image创建虚拟机' },
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/banbu1118' }
+    ],
+    footer: {
+      copyright: 'Copyright © 2023-present Banbu1118'
+    },
+
+    // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    }
+
+
+  }
+})
---
+++ index.md
@@ -0,0 +1,31 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Banbu1118'Blog"
+  text: "一个文档站点"
+  tagline: 持续学习，持续进步
+  image:
+    src: /background.png
+    alt: 背景图片
+  actions:
+    - theme: brand
+      text: Markdown 示例
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+    - theme: alt
+      text: 运维
+      link: /ops/ops
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
